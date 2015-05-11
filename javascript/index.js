410c3cd8b0b26e7aca03bb80af62ba2f5072a6f8
Trevin Hofmann <trevinhofmann@gmail.com>
2015-05-11 18:42:40 -0500

    Add functionality to navigation arrows

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,30 @@
+var locations = ['index'];
+var currentLocation;
+
 $(document).ready(function() {
   navigateTo('index');
+  $('#previous').click(navigatePrevious);
+  $('#next').click(navigateNext);
 });
 
 function navigateTo(page) {
   var page = 'slide/'+page+'.html';
+  currentLocation = page;
   $.get(page, function(html) {
     $('#content').html(html);
   });
+}
+
+function navigatePrevious() {
+  var nextIndex = locations.indexOf(currentLocation) - 1;
+  if (nextIndex >= 0) {
+    navigateTo(locations[nextIndex]);
+  }
+}
+
+function navigateNext() {
+  var nextIndex = locations.indexOf(currentLocation) + 1;
+  if (nextIndex < locations.length) {
+    navigateTo(locations[nextIndex]);
+  }
 }
