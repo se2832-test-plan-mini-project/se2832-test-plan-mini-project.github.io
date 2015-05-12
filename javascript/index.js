944f68d807c20d4f1c34218119f7d97baba5481e
Trevin Hofmann <trevinhofmann@gmail.com>
2015-05-11 20:45:49 -0500

    Remove debugging log

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,6 @@ function index() {
 // Called when the slide is opened
 index.prototype.enter = function() {
   $('h1').css('color', Please.make_color());
-  console.log('entering');
 };
 
 // Called when the slide is closed
