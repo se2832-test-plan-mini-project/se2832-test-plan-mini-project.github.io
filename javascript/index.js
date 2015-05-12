22c01289deb33f9c116ce328dac813558c4a8b94
Trevin Hofmann <trevinhofmann@gmail.com>
2015-05-11 19:09:26 -0500

    Load slides upon page load

--- javascript/index.js
+++ javascript/index.js
@@ -1,33 +1,47 @@
-var locations = ['index', 'slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6', 'slide-7', 'slide-8', 'slide-9'];
+var slideNames = ['index', 'slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6', 'slide-7', 'slide-8', 'slide-9'];
+var slides = {};
 var currentLocation;
 
 $(document).ready(function() {
-  navigateTo('index');
-  $('#previous').click(navigatePrevious);
-  $('#next').click(navigateNext);
+  loadSlides(function() {
+    navigateTo('index');
+    $('#previous').click(navigatePrevious);
+    $('#next').click(navigateNext);
+  });
 });
 
 function navigateTo(page) {
   currentLocation = page;
-  var page = 'slide/'+page+'.html';
   console.log(page);
-  $.get(page, function(html) {
-    $('#content').html(html);
-  });
+  $('#content').html(slides[page]);
 }
 
 function navigatePrevious() {
-  var nextIndex = locations.indexOf(currentLocation) - 1;
+  var nextIndex = slideNames.indexOf(currentLocation) - 1;
   if (nextIndex >= 0) {
-    navigateTo(locations[nextIndex]);
+    navigateTo(slideNames[nextIndex]);
   }
 }
 
 function navigateNext() {
-  console.log(currentLocation);
-  var nextIndex = locations.indexOf(currentLocation) + 1;
-  if (nextIndex < locations.length) {
-    console.log(nextIndex);
-    navigateTo(locations[nextIndex]);
+  var nextIndex = slideNames.indexOf(currentLocation) + 1;
+  if (nextIndex < slideNames.length) {
+    navigateTo(slideNames[nextIndex]);
+  }
+}
+
+function loadSlides(callback) {
+  var completed = 0;
+  for (var i in slideNames) {
+    (function(slideName) {
+      var slide = 'slide/'+slideName+'.html';
+      $.get(slide, function(html) {
+        slides[slideName] = html;
+        completed ++;
+        if (completed == slideNames.length) {
+          callback();
+        }
+      });
+    })(slideNames[i]);
   }
 }
