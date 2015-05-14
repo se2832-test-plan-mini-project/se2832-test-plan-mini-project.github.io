8125d08a920f9b929632fbd01f11a8be3ebdb9c8
dsmiller95 <dsmiller95@gmail.com>
2015-05-14 10:26:57 -0500

    ayyylmao

--- javascript/slide10.js
+++ javascript/slide10.js
@@ -9,9 +9,40 @@ slide10.prototype.enter = function() {
   $.each($('p'), function(){
     $(this).css('color', Please.make_color());
   });
+    ayylmao();
 };
 
 // Called when the slide is closed
 slide10.prototype.exit = function() {
 
 };
+
+
+function ayylmao() {
+    console.log("Ayyyyyylmao");
+    var distance = Math.floor((Math.random() * 800) - 100);
+    $('#ayylmao').css('left', '')
+        .css('right', '');
+    var direction = (Math.floor(Math.random()*2) == 0) ? 'left' : 'right';
+    console.log(direction);
+    console.log(distance);
+    $('#ayylmao').css(direction, distance+'px')
+        .animate({
+            top: '0px'
+        },
+        10000,
+        function() {
+            setTimeout(function() {
+                $('#ayylmao').animate({
+                        top: ($(window).height())+'px'
+                    },
+                    2000,
+                    function() {
+                        setTimeout(ayylmao, 2000);
+                    }
+                );
+            }, 4000);
+        }
+    );
+}
+
