79d7ddd3640d3c787aad099dd67cf6f64b82c65b
dsmiller95 <dsmiller95@gmail.com>
2015-05-14 10:48:37 -0500

    ayylmao

--- javascript/slide10.js
+++ javascript/slide10.js
@@ -9,7 +9,7 @@ slide10.prototype.enter = function() {
   $.each($('p'), function(){
     $(this).css('color', Please.make_color());
   });
-    ayylmao();
+    ayyInit();
 };
 
 // Called when the slide is closed
@@ -17,27 +17,30 @@ slide10.prototype.exit = function() {
 
 };
 
-
-function ayylmao() {
+function ayyInit(){
     for(var i = 0; i < 5; i++) {
-        var distance = Math.floor((Math.random() * 800) - 100);
-        $('#ayylmao' + i).css('left', '')
-            .css('right', '');
-        var direction = (Math.floor(Math.random() * 2) == 0) ? 'left' : 'right';
-        console.log(direction);
-        console.log(distance);
-        $('#ayylmao' + i).css(direction, distance + 'px')
-            .animate({
-                top: '-200px'
-            },
-            Math.random() * 10000,
-            function () {
-                $('#ayylmao' + i).css({
-                    top: ($(window).height()) + 'px'
-                });
-                setTimeout(ayylmao, 10000);
-            }
-        );
+        ayylmao(i);
     }
+}
+
+function ayylmao(ayy) {
+    var distance = Math.floor((Math.random() * 800) - 100);
+    $('#ayylmao' + i).css('left', '')
+        .css('right', '');
+    var direction = (Math.floor(Math.random() * 2) == 0) ? 'left' : 'right';
+    console.log(direction);
+    console.log(distance);
+    $('#ayylmao' + i).css(direction, distance + 'px')
+        .animate({
+            top: '-200px'
+        },
+        Math.random() * 10000,
+        function () {
+            $('#ayylmao' + i).css({
+                top: ($(window).height()) + 'px'
+            });
+            setTimeout(ayylmao(i), 10000);
+        }
+    );
 }
 
