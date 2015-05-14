f01a3b16d19143da4c80c991c4221f95c9eca78a
dsmiller95 <dsmiller95@gmail.com>
2015-05-14 11:10:45 -0500

    ayyyyyyypizza

--- javascript/slide10.js
+++ javascript/slide10.js
@@ -2,6 +2,7 @@
 function slide10() {
 
 }
+var ayyyylmao = false;
 
 // Called when the slide is opened
 slide10.prototype.enter = function() {
@@ -9,12 +10,15 @@ slide10.prototype.enter = function() {
   $.each($('p'), function(){
     $(this).css('color', Please.make_color());
   });
+    ayyyylmao = true;
     ayyInit();
+
 };
+
 
 // Called when the slide is closed
 slide10.prototype.exit = function() {
-
+    ayyyylmao = false;
 };
 
 function ayyInit(){
@@ -24,23 +28,36 @@ function ayyInit(){
 }
 
 function ayylmao(ayy) {
-    var distance = Math.floor((Math.random() * 800) - 100);
-    $('#ayylmao' + ayy).css('left', '')
-        .css('right', '');
-    var direction = (Math.floor(Math.random() * 2) == 0) ? 'left' : 'right';
-    console.log(direction);
-    console.log(distance);
-    $('#ayylmao' + ayy).css(direction, distance + 'px')
-        .animate({
-            top: '-200px'
-        },
-        Math.random() * 10000,
-        function () {
-            $('#ayylmao' + ayy).css({
-                top: ($(window).height()) + 'px'
-            });
-            setTimeout(ayylmao(ayy), 10000);
-        }
-    );
+    if(ayyyylmao) {
+        var distance = Math.floor((Math.random() * 800) - 100);
+        $('#ayylmao' + ayy).css('left', '')
+            .css('right', '');
+        var direction = (Math.floor(Math.random() * 2) == 0) ? 'left' : 'right';
+        console.log(direction);
+        console.log(distance);
+        $('#ayylmao' + ayy).css(direction, distance + 'px')
+            .animate({
+                top: '-200px'
+            },
+            Math.random() * 10000,
+            function () {
+                $('#ayylmao' + ayy).css({
+                    top: ($(window).height()) + 'px'
+                });
+                setTimeout(function () {
+                    ayylmao(ayy);
+                }, 10000);
+            }
+        );
+    }
 }
 
+function brainwash(){
+    var pizza = $('#pizza');
+    pizza.show();
+    setTimeout(function(){
+        pizza.hide();
+        setTimeout(function(){brainwash();}, Math.random() * 3000 + 500);
+    }, 5
+    );
+}
