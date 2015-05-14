7d72a020f484984c95010c7b07ae2fecdcca89b6
dsmiller95 <dsmiller95@gmail.com>
2015-05-14 10:44:12 -0500

    ayylmao

--- javascript/slide10.js
+++ javascript/slide10.js
@@ -19,23 +19,25 @@ slide10.prototype.exit = function() {
 
 
 function ayylmao() {
-    var distance = Math.floor((Math.random() * 800) - 100);
-    $('#ayylmao').css('left', '')
-        .css('right', '');
-    var direction = (Math.floor(Math.random()*2) == 0) ? 'left' : 'right';
-    console.log(direction);
-    console.log(distance);
-    $('#ayylmao').css(direction, distance+'px')
-    .animate({
-            top: '-200px'
-        },
-        Math.random() * 10000,
-        function() {
-            $('#ayylmao').css({
-                    top: ($(window).height())+'px'
-            });
-            setTimeout(ayylmao, 2000);
-        }
-    );
+    for(var i = 0; i < 5; i++) {
+        var distance = Math.floor((Math.random() * 800) - 100);
+        $('#ayylmao' + i).css('left', '')
+            .css('right', '');
+        var direction = (Math.floor(Math.random() * 2) == 0) ? 'left' : 'right';
+        console.log(direction);
+        console.log(distance);
+        $('#ayylmao' + i).css(direction, distance + 'px')
+            .animate({
+                top: '-200px'
+            },
+            Math.random() * 10000,
+            function () {
+                $('#ayylmao' + i).css({
+                    top: ($(window).height()) + 'px'
+                });
+                setTimeout(ayylmao, 2000);
+            }
+        );
+    }
 }
 
